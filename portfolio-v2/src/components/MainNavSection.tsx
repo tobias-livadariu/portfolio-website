843812abe474e4facda4006ce68fb98fb6c2b502
Tobias Livadariu <tobias.livadariu@gmail.com>
2025-08-30 19:22:20 -0400

    Initial button implementation

--- portfolio-v2/src/components/MainNavSection.tsx
+++ portfolio-v2/src/components/MainNavSection.tsx
@@ -1,24 +1,32 @@
 import HorizontalDottedLine from "./HorizontalDottedLine"
 
 function MainNavSection() {
+  const handleClick = (label: string) => {
+    console.log(`${label} clicked`)
+  }
+
   return (
-    <div className="inline-flex flex-col items-center">
-      <HorizontalDottedLine
-        color="gray"
-        dotRadius={1}
-        dotSpacing={6}
-      />
-      <h3>About</h3>
-      <h3>Resume</h3>
-      <h3>Portfolio</h3>
-      <h3>Contact me</h3>
-      <HorizontalDottedLine
-        color="gray"
-        dotRadius={1}
-        dotSpacing={6}
-      />
+    <div className="inline-flex flex-col items-center space-y-1">
+      <HorizontalDottedLine color="gray" dotRadius={1} dotSpacing={6} />
+
+      {["About", "Resume", "Portfolio", "Contact me"].map((label) => (
+        <button
+          key={label}
+          onClick={() => handleClick(label)}
+          className="
+            text-inherit font-pixelemu bg-none border-none cursor-pointer mb-0
+            hover:text-campfire-ash 
+            focus:outline-none focus-visible:text-campfire-ash
+          "
+        >
+          {label}
+        </button>
+      ))}
+
+      <HorizontalDottedLine color="gray" dotRadius={1} dotSpacing={6} />
     </div>
   )
 }
+
 
 export default MainNavSection
