81b4c351f707cf573515d8673606431c4d0dad80
Tobias Livadariu <tobias.livadariu@gmail.com>
2025-08-30 20:09:52 -0400

    Added some pixel icons

--- portfolio-v2/src/components/MainNavSection.tsx
+++ portfolio-v2/src/components/MainNavSection.tsx
@@ -1,3 +1,4 @@
+import { Reply, Play, ChevronLeft, ChevronRight } from "@nsmr/pixelart-react"
 import HorizontalDottedLine from "./HorizontalDottedLine"
 
 function MainNavSection() {
@@ -10,23 +11,33 @@ function MainNavSection() {
       <HorizontalDottedLine color="gray" dotRadius={1} dotSpacing={6} />
 
       {["About", "Resume", "Portfolio", "Contact me"].map((label) => (
-        <button
-          key={label}
-          onClick={() => handleClick(label)}
-          className="
-            text-inherit font-pixelemu bg-none border-none cursor-pointer mb-0
-            hover:text-campfire-ash 
-            focus:outline-none focus-visible:text-campfire-ash
-          "
-        >
-          {label}
-        </button>
+        <div key={label} className="group flex items-center">
+          <Reply
+            className="text-campfire-ash visible group-hover:visible group-focus-within:visible rotate-180 mr-[2px]"
+            size={24}
+          />
+
+          <button
+            onClick={() => handleClick(label)}
+            className="
+              text-inherit font-pixelemu bg-none border-none cursor-pointer
+              hover:text-campfire-ash
+              focus:outline-none focus-visible:text-campfire-ash
+            "
+          >
+            {label}
+          </button>
+
+          <Reply
+            className="text-campfire-ash visible group-hover:visible group-focus-within:visible"
+            size={24}
+          />
+        </div>
       ))}
 
       <HorizontalDottedLine color="gray" dotRadius={1} dotSpacing={6} />
     </div>
   )
 }
-
 
 export default MainNavSection
