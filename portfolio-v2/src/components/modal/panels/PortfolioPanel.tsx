a8fbe7b06e1c0a7828ce810976ab8b25614dc3f9
Tobias Livadariu <tobias.livadariu@gmail.com>
2025-09-11 18:20:41 -0400

    More progress towards portfolio

--- portfolio-v2/src/components/modal/panels/PortfolioPanel.tsx
+++ portfolio-v2/src/components/modal/panels/PortfolioPanel.tsx
@@ -4,27 +4,13 @@ export default function AboutPanel() {
   return (
     <div>
       <ImageTextCaption
-        imageUrl="/images/me.png"
-        imageAlt="Portrait of me!"
-        imageCaption="A drawing of my face made by a friend"
+        imageUrl="/images/ideanotion-banner.png"
+        imageAlt="IdeaNotion's banner"
+        imageCaption="The sign of my last employer!"
       >
-        <p className="mt-2 md:mt-0 text-[20px] font-pressstart"># ABOUT ME</p>
-        <p className="text-[16px]">## TOBIAS LIVADARIU</p>
-
-        <p>
-          I’m a 2nd-year Software Engineering student at the University of Waterloo, 
-          passionate about building tools at the intersection of frontend design and 
-          backend systems. I’ve worked on projects like DealerAI’s Admin Portal and 
-          Food Banks Canada’s Exchange platform.
-        </p>
-
-        <p>
-          Outside of coding, I enjoy getting lost in books and challenging myself in 
-          the gym. Reading keeps my imagination sharp, and lifting keeps me grounded.
-        </p>
-
-        <p className="text-[16px]">## SKILLS & INTERESTS</p>
-
+        <p className="mt-2 md:mt-0 text-[20px] font-pressstart"># IDEANOTION DEVELOPMENT INC.</p>
+        <p className="mb-2">My last internship was an incredible experience.</p>
+        <p className="mb-5"><span></span>I had the opportunity to work on three major projects:</p>
         <ul className="list-disc pixel-list pl-5">
           <li>Frontend: React, TypeScript, Tailwind, Pixi.js</li>
           <li>Backend: C#, .NET, SQL, CosmosDB, Azure</li>
@@ -32,6 +18,9 @@ export default function AboutPanel() {
           <li>Hobbies: Reading fantasy novels, lifting weights</li>
         </ul>
       </ImageTextCaption>
+
+      <p>&#9633;</p>
+      <hr className="pixel-divider my-6" aria-hidden="true" />
 
       <ImageTextCaption
         imageUrl="/images/me.png"
